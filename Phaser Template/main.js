5064cd2935424dbbdf54b7b94ae7840aee84d990
William Davis <vixvaldi@gmail.com>
2016-09-27 22:02:41 -0700

    Template for Phaser
    This template is the template I use for most of my Phaser Games.

---
+++ Phaser Template/main.js
@@ -0,0 +1,13 @@
+var game = new Phaser.Game(window.innerWidth * window.devicePixelRatio, window.innerHeight * window.devicePixelRatio, Phaser.CANVAS, 'gameCanvas', {preload: preload, create: create, update: update});
+
+function preload(){
+
+}
+
+function create(){
+
+}
+
+function update(){
+
+}
